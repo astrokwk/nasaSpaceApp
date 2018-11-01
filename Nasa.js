c7ae49df0d310e83251512cca328248ee9cae899
astrokwk <awykmok@gmail.com>
2018-11-01 16:35:40 -0400

    add animation to sections

--- Nasa.js
+++ Nasa.js
@@ -125,3 +125,19 @@ xhr.send();
 	// xhr.send();
 // });
 
+
+
+
+
+// myID = document.getElementById("myID");
+
+// var myScrollFunc = function() {
+//   var y = window.scrollY;
+//   if (y >= myID.scrollY) {
+//     myID.className = "bottomMenu show"
+//   } else {
+//     myID.className = "bottomMenu hide"
+//   }
+// };
+
+// window.addEventListener("scroll", myScrollFunc);
