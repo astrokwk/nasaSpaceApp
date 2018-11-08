edf18759a9f6e8be6f516669c1965731eead414d
astrokwk <awykmok@gmail.com>
2018-11-07 21:48:32 -0500

    initiate

--- mars-rover.js
+++ mars-rover.js
@@ -1,4 +1,7 @@
+var rover = document.getElementById('rover');
 var apiKey = 'OFxgFiHTSBNXJIKYfAiYq0LrcjOZWrL53tBbpg3o';
+var roverInfo = document.getElementById('roverInfo');
+
 
 var xhr = new XMLHttpRequest();
 
@@ -7,6 +10,34 @@ xhr.onload = function() {
     var myObj = JSON.parse(this.response);
     console.log(xhr.status);
     console.log(myObj);
+
+    var k = '<tbody>';
+
+    k += '<tr>';
+    k += '<td> Rover: ' + myObj.photo_manifest.name + '</td>';
+    k += '<td> Status: ' + '<span id="status">' + myObj.photo_manifest.status.toUpperCase(); + '</span>' + '</td>';
+    k += '<td> Total Photos: ' + myObj.photo_manifest.total_photos + '</td>';
+    k += '</tr> <tr>';
+    k += '<td> Launch Date: ' + myObj.photo_manifest.launch_date + '</td>';
+    k += '<td> Landing Date: ' + myObj.photo_manifest.landing_date + '</td>';
+    k += '<td> Max Sol: ' + myObj.photo_manifest.max_sol + '</td>';
+    k += '</tr> '
+    k+= '</tbody>';
+    roverInfo.innerHTML = k;
+
+    if(myObj.photo_manifest.status === "active") {
+      document.getElementById('status').style.color = "#52D017";
+    } else {
+      document.getElementById('status').style.color = "#3090C7";
+    }
+
+    var sol = document.getElementById('sol');
+    var solLabel = document.getElementById('solLabel');
+    
+    sol.style.display = "block";
+    solLabel.innerHTML = 'Pick a number between 0 and ' + myObj.photo_manifest.max_sol;
+    
+
   }  else {
     console.log('The Request Failed');
   }
@@ -14,5 +45,16 @@ xhr.onload = function() {
 
 }
 
-xhr.open("GET", "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=" + apiKey);
-xhr.send();
+// xhr.open("GET", "https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1000&api_key=" + apiKey);
+// xhr.send();
+
+rover.addEventListener('change', selection);
+
+function selection(select) {
+  var chosenRover = rover.options[rover.selectedIndex].value;
+
+  console.log(chosenRover);
+  // xhr.open('GET', 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + chosenRover + '/photos?sol=1000&api_key=' + apiKey);
+  xhr.open('GET', 'https://api.nasa.gov/mars-photos/api/v1/manifests/' + chosenRover + '?&api_key=' + apiKey);
+  xhr.send();
+}
