b5342a725d366967e49288b1980d0e9629831e46
astrokwk <awykmok@gmail.com>
2018-11-19 02:29:53 -0500

    remove code

--- Nasa.js
+++ Nasa.js
@@ -8,7 +8,6 @@ var header = document.getElementById('header');
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 	// some code..
-	console.log('mom');
 	header.style.fontSize = "30px";
 	header.style.padding = "20px 10px 10px";
  } else {
@@ -31,7 +30,6 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 
  function closeNav() {
 	 document.getElementById('sidenav').style.width = "0";
-	 console.log('Chicken');
  }
 
 
@@ -40,9 +38,7 @@ var xhr = new XMLHttpRequest();
 
 xhr.onload = function() {
 	if(xhr.status >= 200 && xhr.status <300) {
-		// console.log('success', xhr);
 		var myObj = JSON.parse(this.response);
-		// console.log(myObj);
 		
 		for (var i = 0; i < myObj.articles.length; i++) {
 			var str = myObj.articles[i].title;
@@ -58,14 +54,12 @@ xhr.onload = function() {
 			}
 
 			if(contains(str, arr)){
-				// console.log(myObj.articles[i].title);
 				var newsbox = document.createElement('div');
 				var newsimage = document.createElement('img');
 				var newslink = document.createElement('a');
 
 				newsbox.setAttribute('class','newsbox');
 				newsbox.setAttribute('id', 'newsboxid');
-				console.log(myObj.articles[i]);
 				newslink.setAttribute('class', 'newslink');
 				newslink.setAttribute('href', myObj.articles[i].url);
 				newslink.setAttribute('target', "_blank");
